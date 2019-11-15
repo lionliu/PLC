4a158de3d3bce8806e495ba6799f501012e62022
Leão Liu Masur <lionliu@pop-os.localdomain>
2019-11-15 20:07:26 -0300

    Adicionado revisao 2EE

---
+++ Rev2EE/Primeira.java
@@ -0,0 +1,18 @@
+import java.util.Vector
+
+public class SafeVector<E> {
+
+    Vector<E> vec = new Vector<E>();
+    
+    public synchronized void get() {
+
+    }
+
+    public synchronized void set() {
+        
+    }
+
+    public synchronized void swap() {
+        
+    }
+}
