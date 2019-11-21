c68e4dcfa7e5a2933ac90e546855e6e71a85f98f
Leão Liu Masur <lionliu@pop-os.localdomain>
2019-11-21 08:23:40 -0300

    Questao de lua certa

--- 2EE/2EE20191/Quarta.lua
+++ 2EE/2EE20191/Quarta.lua
@@ -1,15 +1,19 @@
 
 function newPoly (fatores)
-    qtFatores = Table.getn(fatores)
+    qtFatores = #fatores
     novaFuncao = function(x)
         it = qtFatores
         fatores = fatores -- Não sei se ele ainda armazenaria os fatores depois de ser retornado
-        total = fatores[qtFatores]
-        for i=1, (it - 1) do
-            total = total + fatores[i] * (x ^ (i-1))
+        total = 0
+        for i=1, it  do
+            total = total + fatores[i] * (x ^ (it - i))
         end
         return total
     end
     return novaFuncao
 end
-    
+
+f = newPoly({3,0,1})
+print(f(0))
+print(f(5))
+print(f(10))
