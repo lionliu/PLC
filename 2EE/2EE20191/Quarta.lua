a64e3317a712935018bb3f95be48d82c3f17c3ff
Leao Liu Masur <llm2@cin.ufpe.br>
2019-11-21 07:03:28 -0300

    Adicionado questao de lua

--- 2EE/2EE20191/Quarta.lua
+++ 2EE/2EE20191/Quarta.lua
@@ -1,3 +1,15 @@
 
-function newPoly () 
+function newPoly (fatores)
+    qtFatores = Table.getn(fatores)
+    novaFuncao = function(x)
+        it = qtFatores
+        fatores = fatores -- Não sei se ele ainda armazenaria os fatores depois de ser retornado
+        total = fatores[qtFatores]
+        for i=1, (it - 1) do
+            total = total + fatores[i] * (x ^ (i-1))
+        end
+        return total
+    end
+    return novaFuncao
+end
     
