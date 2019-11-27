90ee0bcecd38bc531119094bf005fa132da39319
Leao Liu Masur <llm2@cin.ufpe.br>
2019-11-27 08:53:21 -0300

    Incremento da classe Jogo na segunda da lista

--- Lista/Segunda.java
+++ Lista/Segunda.java
@@ -3,11 +3,9 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-// TODO: Fazer consumidores como os jogadores e um produtor de cadeiras. Na primeira rodada, criar N consumidores e produzir N-1 cadeiras, e a cada rodada ir decrementando o N, até que reste 1 jogador. 
-// O produtor pode gerar um atomic integer = (qt jogadores-1), sendo que cada jogador(consumidor) subtrai 1 do valor e coloca a flag sentado = 1
+
 public class Segunda {
     public static void main(String[] args) {
-        UtilidadePublica up = new UtilidadePublica();
         int qtJogadores;
         Scanner s = new Scanner(System.in);
         qtJogadores = s.nextInt();
@@ -22,12 +20,12 @@ public class Segunda {
                 (new Thread(j.jogadores.get(i))).start();
             }
 
-            while(!up.isAllCadeirasOcupadas(j.cadeiras)) {
+            while(!j.isAllCadeirasOcupadas()) {
                 // Enquanto todas as cadeiras não estiverem ocupadas o jogo vai rolando
                 
             }
 
-            up.reiniciaJogo(j.cadeiras, j.jogadores);
+            j.reiniciaJogo(j.cadeiras, j.jogadores);
         }
         System.out.println(j.jogadores.get(0) + " ganhou");
     }
@@ -40,7 +38,6 @@ class Jogador implements Runnable{
     public String name;
     public Boolean sentado;
     public Jogo j;
-    UtilidadePublica up;
 
     public Jogador(String n, Jogo j){
         this.name = n;
@@ -49,15 +46,7 @@ class Jogador implements Runnable{
 
     public void run() {
         // Roda o run até que ele sente ou todas as cadeiras estejam ocupadas
-        while(!(this.sentado) && !this.up.isAllCadeirasOcupadas(this.j.cadeiras)) {
-            for(int i = 0; i < this.j.cadeiras.size(); i++) {
-                // Se a caderia não estiver ocupada,ocupa ela
-                if(!this.j.cadeiras.get(i).get()) {
-                    this.j.cadeiras.get(i).set(true);
-                    this.sentado = true;
-                }
-            }
-        }
+        j.playGame(this);
     }
 
     public String toString() {
@@ -65,36 +54,6 @@ class Jogador implements Runnable{
     }
 
 
-}
-
-class UtilidadePublica {
-    public boolean isAllCadeirasOcupadas(List<AtomicBoolean> cadeiras) {
-        for (int i = 0; i < cadeiras.size(); i++) {
-            System.out.println(i);
-            if (!cadeiras.get(i).get()) {
-                return false;
-            }
-        }
-        return true;
-    }
-    public void reiniciaJogo(List<AtomicBoolean> cadeiras, List<Jogador> jogadores) {
-        // remove uma cadeira
-        cadeiras.remove(0);
-        // Tira os jogadores das cadeiras.
-        for(int i = 0; i < cadeiras.size(); i++) {
-            cadeiras.get(i).set(false);
-        }
-        for (int i = 0; i < jogadores.size(); i++) {
-
-            if(!jogadores.get(i).sentado) {
-                // Remove o jogador em pé
-                System.out.println(jogadores.get(i).name + " foi eliminado");
-                jogadores.remove(i);
-            }
-            // coloca os jogadores de pé de volta
-            jogadores.get(i).sentado = false;
-        }
-    }
 }
 
 class Producer implements Runnable {
@@ -106,12 +65,7 @@ class Producer implements Runnable {
     }
 
     public void run() {
-        // Gerar N-1 cadeiras
-        synchronized(j.cadeiras) {
-            for (int i = 0; i < (j.N - 1); i++) {
-                j.cadeiras.add(new AtomicBoolean());
-            }
-        }
+        j.createChairs();
     }
 }
 
@@ -136,5 +90,94 @@ class Jogo {
             l.add(new Jogador("Jogador " + i, this));
         }
         return l;
+    }
+
+    // take
+    public synchronized void playGame(Jogador jogador) {
+        // Só começa a jogar depois de todas as cadeiras serem criadas
+        while(this.cadeiras.size() != (this.N-1)) {
+            try {
+                wait();
+            } catch(Exception e) {}
+        }
+
+        while (!(jogador.sentado) && !this.isAllCadeirasOcupadas()) {
+            for (int i = 0; i < this.cadeiras.size(); i++) {
+                // Se a caderia não estiver ocupada,ocupa ela
+                if (!this.cadeiras.get(i).get()) {
+                    this.cadeiras.get(i).set(true);
+                    jogador.sentado = true;
+                }
+            }
+        }
+
+        notifyAll();
+
+    }
+
+    // put
+    public synchronized void createChairs() {
+        for (int i = 0; i < (this.N - 1); i++) {
+            this.cadeiras.add(new AtomicBoolean());
+        }
+        // Notifica aos jogadores que as cadeira estão disponiveis
+        notifyAll();
+    }
+
+    public synchronized boolean isAllCadeirasOcupadas() {
+        while (this.cadeiras.size() != (this.N - 1)) {
+            try {
+                wait();
+            } catch (Exception e) {
+            }
+        }
+
+        for (int i = 0; i < this.cadeiras.size(); i++) {
+            System.out.println(i);
+            if (!this.cadeiras.get(i).get()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public synchronized void reiniciaJogo(List<AtomicBoolean> cadeiras, List<Jogador> jogadores) {
+
+        while(this.isAllCadeirasOcupadas()) {
+            try {
+                wait();
+            } catch (Exception e) {}
+        }
+
+
+        int eliminado = 0;
+        
+        // Tira os jogadores das cadeiras.
+        for (int i = 0; i < cadeiras.size(); i++) {
+            cadeiras.get(i).set(false);
+        }
+        for (int i = 0; i < jogadores.size(); i++) {
+
+            if (!jogadores.get(i).sentado) {
+                // Remove o jogador em pé
+                eliminado = i;
+            } else {
+                // coloca os jogadores de pé de volta
+                jogadores.get(i).sentado = false;
+            }
+            
+        }
+
+        System.out.println(jogadores.get(eliminado).name + " foi eliminado");
+        jogadores.remove(eliminado);
+        this.N--;
+        // remove uma cadeira
+        cadeiras.remove(0);
+
+        if(jogadores.size() == 1) {
+            System.out.println(this.jogadores.get(0).name + "ganhou");
+        } else {
+            // TODO: Fazer reinicio do jogo aqui
+        }
     }
 }
